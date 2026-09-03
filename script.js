69c999906d56ec325f1d2df0e0cf11bbbfd1def8
Debora <debora.cyriaco@escola.pr.gov.br>
2026-09-03 11:03:50 +0000

    "linkar" css

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-async function buscarclima(cidade)(
+async function buscarclima(cidade){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${CHAVE_API}&units=metric&lang=pt_br`
     const resposta = await fetch(url);
     const dados = await resposta.json()
-)
+}
+
+const botaoBuscar = document.getElementById("btnBuscar");
+const campoCidade = document.getElementById("cidade");
+const divResultado = document.getElementById("resultado");
+
+botaoBuscar.addEventListener("click", async function(){
+    const cidade = campoCidade.value;
+    
+    if (cidade == ""){
+        return;
+    }
+})
