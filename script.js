e766352124786a5960df254708d818c6aa925e62
Debora <debora.cyriaco@escola.pr.gov.br>
2026-09-14 11:57:55 +0000

    correção bugs

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ async function buscarclima(cidade){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${CHAVE_API}&units=metric&lang=pt_br`
     const resposta = await fetch(url);
     const dados = await resposta.json()
+    return dados; /* tem que retornar os dados. Agora a função entrega a resposta da API para a sua variável dados de fora */
 }
 
 const botaoBuscar = document.getElementById("btnBuscar");
@@ -17,17 +18,17 @@ botaoBuscar.addEventListener("click", async function(){
 
     const dados = await buscarclima(cidade);
 
-    if (dados.cod === "404"){
+    if (dados.cod === "404"){ /* tem igualdade me js é com 3 === */
         divResultado.innerHTML = "<p>Cidade não encontrada.</p>";
         return;
     }
 
     divResultado.innerHTML = `
         <div class= "card-clima">
-            <h3>$(dados.name)</h3>
-            <p>$(dados.weather[0].description)</p>
-            <p><strong>$(dados.main.temp)ºC</strong></p>
-            <p>Sensação: $(dados.main.feels_like)ºC</p>
+            <h3>${dados.name}</h3>
+            <p>${dados.weather[0].description}</p>
+            <p><strong>${dados.main.temp}ºC</strong></p>
+            <p>Sensação: ${dados.main.feels_like}ºC</p>
         </div>
-    `
-})
+    `;
+});
